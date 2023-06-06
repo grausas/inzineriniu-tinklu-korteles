fc19c2b06cc10b0aad40408512666d08a33cfd58
Marius Pikzirnis <grausas@gmail.com>
2023-06-06 11:50:54 +0300

    removed unused code

--- src/queries/queryFeatures.tsx
+++ src/queries/queryFeatures.tsx
@@ -1,27 +1,3 @@
-// export async function queryFeatures(layer: __esri.FeatureLayer) {
-//   let allFeatures: __esri.Graphic[] = [];
-//   let exceededTransferLimit = true;
-//   const params = {
-//     where: "1=1",
-//     returnGeometry: false,
-//     outFields: ["*"],
-//     start: 0,
-//     num: 2000,
-//   };
-
-//   while (exceededTransferLimit) {
-//     const results = await layer.queryFeatures(params);
-//     allFeatures = allFeatures.concat(results.features);
-//     exceededTransferLimit = results.exceededTransferLimit;
-
-//     if (exceededTransferLimit) {
-//       params.start += params.num;
-//     }
-//   }
-
-//   return allFeatures;
-// }
-
 export async function queryFeatures(
   layer: __esri.FeatureLayer,
   page: number,
